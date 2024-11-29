3c2cf4ee37ca5a53a4ad13fbd7ca21587ca7f311
Maciel Suassuna <macielsuassuna14@gmail.com>
2024-11-29 01:54:27 -0300

    feat: add User Decorated, Calls and Subjects objects

--- core/meetlink/admin.py
+++ core/meetlink/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Call, Subject, User
+
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin) :
+  list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+  list_filter = ('role', 'is_staff', 'is_active')
+
+@admin.register(Call)
+class CallAdmin(admin.ModelAdmin) :
+  list_display = ('responsible', 'subject', 'created_at')
+  search_fields = ('responsible__username', 'subject__name', 'description')
+  list_filter = ('created_at',)
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin) :
+  list_display = 'name',
+  search_fields = 'name',
--- core/meetlink/models.py
+++ core/meetlink/models.py
@@ -1,3 +1,73 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 # Create your models here.
+
+class Role(models.TextChoices) :
+  SUPERADMIN = 'SUPERADMIN', 'Super Administrador'
+  MANAGER = 'MANAGER', 'Gestor'
+  TOTEM = 'TOTEM', 'Totem'
+  INTERPRETER = 'INTERPRETER', 'Intérprete'
+
+class User(AbstractUser) :
+  role = models.CharField(
+    max_length=20,
+    choices=Role.choices,
+    default=Role.MANAGER
+  )
+
+  groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",
+        blank=True,
+    )
+  
+  user_permissions = models.ManyToManyField(
+      Permission,
+      related_name="custom_user_permissions_set",
+      blank=True,
+  )
+  
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def has_dashboard_access(self) :
+    return self.role == Role.SUPERADMIN
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nome')
+    description = models.TextField(blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta :
+        verbose_name = 'Assunto'
+        verbose_name_plural = 'Assuntos'
+
+
+class Call(models.Model):
+    responsible = models.ForeignKey(
+        'User', 
+        on_delete=models.CASCADE, 
+        related_name='calls',
+        verbose_name='Responsável'
+    )
+    subject = models.ForeignKey(
+        'Subject', 
+        on_delete=models.SET_NULL,
+        null=True, 
+        blank=True,
+        verbose_name='Assunto'
+    )
+    description = models.TextField(verbose_name='Descrição')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de Criação')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Data de Atualização')
+
+    def __str__(self):
+        return f"Call by {self.responsible.username} on {self.created_at}"
+    
+    class Meta :
+       verbose_name = 'Chamada'
+       verbose_name_plural = 'Chamadas'
