d73e99338bb99e747df553dc4c13c4136940943e
Maciel Suassuna <macielsuassuna14@gmail.com>
2024-12-18 17:23:47 -0300

    feat: refactor call_view to dedicated file.

--- core/meetlink/urls.py
+++ core/meetlink/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from meetlink.domain.call import call_view
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -8,7 +9,8 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("dashboard/", views.dashboard, name="dashboard"),
-    path("calls/", views.calls_index, name="calls_index"),
-    path("calls/<int:id>", views.CallsEdit.as_view(), name="calls_edit"),
+    path("calls/", call_view.calls_index, name="calls_index"),
+    path("calls/<int:id>", call_view.CallsEdit.as_view(), name="calls_edit"),
+    path("api/calls", call_view.CallsApi.as_view(), name="calls_api_create"),
     # path("create_call/", views.create_call, name="create_call")
 ]
--- core/meetlink/views.py
+++ core/meetlink/views.py
@@ -2,17 +2,13 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import check_password
 from django.shortcuts import redirect, render
-from django.utils.decorators import method_decorator
 from django.views import View
-from meetlink.domain.call.call_adapter import GoogleMeetAdapter
-from meetlink.domain.call.call_repository import CallRepository
-from meetlink.domain.call.call_service import CallService
-from meetlink.domain.subject.subject_repository import SubjectRepository
-from meetlink.domain.user.user_repository import UserRepository
 
 from .domain.user.user_exceptions import (
-    LoginDataIsInvalidException, UserEmailOrPasswordIsInvalidException)
-from .forms import EditCallForm, LoginForm
+    LoginDataIsInvalidException,
+    UserEmailOrPasswordIsInvalidException,
+)
+from .forms import LoginForm
 from .models import Role, User
 
 # Create your views here.
@@ -85,63 +81,3 @@ def logout_view(request):
         logout(request)
 
     return redirect("index")
-
-
-@login_required
-def create_call(request):
-    call_adapter = GoogleMeetAdapter()
-
-    call = call_adapter.create_call()
-
-    return render(request, "create_call.html", {"call_uri": call.call_uri})
-
-
-@login_required
-def calls_index(request):
-    call_repository = CallRepository()
-    user_repository = UserRepository()
-    subject_repository = SubjectRepository()
-
-    call_service = CallService(call_repository, user_repository, subject_repository)
-
-    calls = call_service.get_all()
-
-    return render(request, "calls/index.html", {"calls": calls})
-
-
-class CallsEdit(View):
-    def __init__(self):
-        self.call_repository = CallRepository()
-        self.user_repository = UserRepository()
-        self.subject_repository = SubjectRepository()
-
-        self.call_service = CallService(
-            self.call_repository, self.user_repository, self.subject_repository
-        )
-
-    @method_decorator(login_required)
-    def get(self, request, id):
-        call = self.call_service.get(id)
-        subjects = self.subject_repository.get_all()
-
-        return render(
-            request,
-            "calls/edit.html",
-            {
-                "call": call,
-                "subjects": subjects,
-                "error": request.session.pop("error", None),
-                "success": request.session.pop("success", None),
-            },
-        )
-
-    @method_decorator(login_required)
-    def post(self, request, id):
-        try:
-            self.call_service.update(id, EditCallForm(request.POST))
-            request.session["success"] = "Dados da Chamada atualizados com sucesso!"
-
-        except Exception as e:
-            request.session["error"] = str(e)
-
-        return redirect("calls_edit", id=id)
