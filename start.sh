6d1cb9a6dbadd3afd9978fdc729819269afa7bab
Maciel Suassuna <macielsuassuna14@gmail.com>
2025-01-23 23:09:37 -0300

    feat

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ "$ENV" = "production" ]; then
   echo "Iniciando Daphne em produção com SSL..."
-  daphne -e ssl:443:privateKey=/app/certbot/conf/live/totemvirtual.com.br/privkey.pem:certKey=/app/certbot/conf/live/totemvirtual.com.br/fullchain.pem core.asgi:application
+  daphne -b 0.0.0.0 -p 8001 -e ssl:443:privateKey=/app/certbot/conf/live/totemvirtual.com.br/privkey.pem:certKey=/app/certbot/conf/live/totemvirtual.com.br/fullchain.pem core.asgi:application
 else
   echo "Iniciando Daphne em desenvolvimento sem SSL..."
   daphne -b 0.0.0.0 -p 8001 core.asgi:application
