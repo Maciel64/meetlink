baa23d53de5512a852ddf06aca7c8a4c37803b4a
Maciel Suassuna <macielsuassuna14@gmail.com>
2025-01-23 22:02:01 -0300

    trying with AllowedHostsOriginValidator

--- core/core/asgi.py
+++ core/core/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 from meetlink.domain.call.call_consumer import CallConsumer
@@ -21,12 +22,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                [
-                    re_path("ws/calls/?$", CallConsumer.as_asgi()),
-                    re_path("ws/meetings/?$", MeetingConsumer.as_asgi()),
-                ]
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    [
+                        re_path("ws/calls/?$", CallConsumer.as_asgi()),
+                        re_path("ws/meetings/?$", MeetingConsumer.as_asgi()),
+                    ]
+                )
             )
         ),
     }
